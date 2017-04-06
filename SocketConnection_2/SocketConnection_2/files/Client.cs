4835e7d8e467eaf0378cb3528e7c5bbcb2456532
Zheng Fei <zf6792@163.com>
2017-04-06 18:26:27 +0800

    Created class Connection(for server) and Client

---
+++ SocketConnection_2/SocketConnection_2/files/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SocketConnection_2.files
+{
+    class Client
+    {
+        //The maxiumum length of a message
+        public const int BufferSize = 1024;
+        public Socket socket;
+        bool isConnected = false;
+        private string hostAddress;
+        private int port;
+        //This buffer is for receiving message
+        byte[] readBuff = new byte[BufferSize];
+        //Store the message received just now
+        public string messageReceived;
+        public void Connect()
+        {
+            //Create a new socket, then connect it to the host.
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(hostAddress, port);
+        }
+
+        private void ReceiveCallBack(IAsyncResult asyncRe)
+        {
+            int msgSize = socket.EndReceive(asyncRe);
+            messageReceived = System.Text.Encoding.UTF8.GetString(readBuff, 0, msgSize);
+            //continue receiving
+            socket.BeginReceive(readBuff, 0, BufferSize, SocketFlags.None, ReceiveCallBack, null);
+        }
+        public void Send(string sentingMsg)
+        {
+            byte[] bytes = System.Text.Encoding.Default.GetBytes(sentingMsg);
+            if(!isConnected)
+            {
+                return;
+            }
+            socket.Send(bytes);
+        }
+    }
+}
---
+++ SocketConnection_2/SocketConnection_2/files/Server.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketConnection_2.files
+{
+    class Connection
+    {
+        public const int BufferSize = 1024;
+        public Socket socket;
+        public bool isUsing = false;
+        public byte[] ReadBuff = new byte[1024];
+        public int bufferCount = 0;
+        public void Init(Socket clientSocket)
+        {
+            socket = clientSocket;
+            isUsing = true;
+        }
+        public int RemainBuff()
+        {
+            return BufferSize - bufferCount;
+        }
+        public string GetAddress()
+        {
+            if(!isUsing)
+            {
+                return "Socket dose not created.";
+            }
+            return socket.RemoteEndPoint.ToString();
+        }
+        public void Close()
+        {
+            if(!isUsing)
+            {
+                return;
+            }
+            socket.Close();
+            isUsing = false;
+        }
+    }
+    class Server
+    {
+    }
+}
